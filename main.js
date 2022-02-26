c473ef5509bd8c8bddea760881cb00949f85f8af
chlqls <99929277+chlqls@users.noreply.github.com>
2022-02-26 22:42:33 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const toggleBtn = document.querySelector('.navbar__toggleBtn');
 const menu = document.querySelector('.navbar__menu');
 const icons = document.querySelector('.navbar__icons');
 
-toggleBtn.addEventListener('click', () => {
+toggleBtn.addEventListener('click', () => { //클릭할 때마다 실행
   menu.classList.toggle('active');
   icons.classList.toggle('active');
 });
